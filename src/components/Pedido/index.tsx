f79f12bad8ce4426dea9094116397ced83544e00
Breno Alves Ribeiro <brenoalvees445@gmail.com>
2023-11-08 22:43:13 -0300

    Edit and delete button in Pedidos

--- src/components/Pedido/index.tsx
+++ src/components/Pedido/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 
-import { Container, InfoPedido } from "./styles";
+import { Container, InfoPedido, EditButton, DeleteButton } from "./styles";
 
-interface Props{
+interface Props {
   name: string;
   valor: number;
   status: string;
@@ -10,18 +10,22 @@ interface Props{
 }
 
 
-const Pedido: React.FC<Props> = ({name, valor, itens, status}) => {
-  
+const Pedido: React.FC<Props> = ({ name, valor, itens, status }) => {
+
   const itensFormatados = itens.join(", ");
 
   return (
     <Container>
-    <InfoPedido>
-    <p>{name}</p>
-    <p>{itensFormatados}</p>
-    <p>R$ {valor}</p>
-    <p>{status}</p>
-    </InfoPedido>
+      <InfoPedido>
+        <p>{name}</p>
+        <p>{itensFormatados}</p>
+        <p>R$ {valor}</p>
+        <p>{status}</p>
+        <div>
+          <EditButton />
+          <DeleteButton />
+        </div>
+      </InfoPedido>
     </Container>
   );
 };
--- src/components/Pedido/styles.ts
+++ src/components/Pedido/styles.ts
@@ -1,4 +1,8 @@
-import styled, {css} from "styled-components";
+import styled, { css } from "styled-components";
+
+import { Edit } from "@styled-icons/boxicons-solid/Edit"
+import { DeleteOutline } from "@styled-icons/typicons/DeleteOutline"
+
 
 export const Container = styled.div`
   margin: 0px 0px 0px 0px;
@@ -18,7 +22,7 @@ text-align: center;
   gap: 0.5rem;
   max-height: 0.5rem;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr 0.5fr;
   font-size: 17px;
   >p{
     border-radius: 1rem;
@@ -36,4 +40,29 @@ text-align: center;
     height: 5rem;
     padding-left: 2rem;
   }
+  >div{
+    gap: 17px;
+    border-radius: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #2B2B2B;
+    background-color: #C0C0C0;
+    margin: 0;
+    font-size: 18px;
+    font-weight: bold;
+    width:100%;
+    height: 5rem;
+  }
+`
+
+export const DeleteButton = styled(DeleteOutline)`
+  color: red;
+  width: 40px;
+`;
+
+export const EditButton = styled(Edit)`
+  color: blue;
+  width: 32px;
 `;
